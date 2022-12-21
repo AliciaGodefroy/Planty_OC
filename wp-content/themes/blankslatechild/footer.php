3a72e935c36584c5d07ecda8714d137e8f5aa8e5
Alicia Godefroy <alicia.godefroy@fk-agency.com>
2022-12-21 12:42:21 +0100

    up

--- wp-content/themes/blankslatechild/footer.php
+++ wp-content/themes/blankslatechild/footer.php
@@ -1,8 +1,10 @@
 </main>
 <?php get_sidebar(); ?>
 </div>
-<footer id="footer" role="contentinfo">
-
+<footer id="footer" class ="footer">
+	<div class="footer_ctn">
+		<a href="#" class="footer_link">Mentions Légales</a>
+	</div>
 </footer>
 </div>
 <?php wp_footer(); ?>
